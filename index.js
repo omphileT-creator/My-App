d6ad8e597c2e1e3cc3f7caf60da3ce183f79a392
omphileT-creator <155839199+omphileT-creator@users.noreply.github.com>
2024-01-08 16:35:11 +0200

    added search engine and API

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+    let apiKey = "4a7075563b0eebo45t378ca235ffa601";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+    axios.get(apiUrl).then(updateWeather);
+}
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Sandton");
